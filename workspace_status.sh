243e265b5d661749c1c5fc6b15c809c72491cf42
Manuel Naranjo <manuel.naranjo@booking.com>
2023-09-13 11:24:46 +0200

    bazel: adding bazelrc and workspace status
    adding a workspace status that enables build buddy remote cache,
    together with a workspace status

---
+++ workspace_status.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+function remove_url_credentials() {
+  which perl >/dev/null && perl -pe 's#//.*?:.*?@#//#' || cat
+}
+
+repo_url=$(git config --get remote.origin.url | remove_url_credentials)
+commit_sha=$(git rev-parse HEAD)
+git_branch=$(git rev-parse --abbrev-ref HEAD)
+git_tree_status=$(git diff-index --quiet HEAD -- && echo 'Clean' || echo 'Modified')
+
+cat << EOF
+STABLE_DATE_TODAY $(date -u +%d-%m-%Y)
+REPO_URL ${repo_url}
+STABLE_REPO_URL ${repo_url}
+COMMIT_SHA ${commit_sha}
+GIT_BRANCH ${git_branch}
+GIT_TREE_STATUS ${git_tree_status}
+EOF
